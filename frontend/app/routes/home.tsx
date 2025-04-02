bb9104268f54e310000e5df9bd4dd122361c060e
dukunuu <dukk@nashatech.com>
2025-04-02 20:08:18 +0800

    Front-end setup

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,5 +1,5 @@
+import { Welcome } from "~/components/common";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
